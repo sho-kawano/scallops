48a792fb25356acc71e385b87c41aef542637eaf
Ricardo Silva Tavares de Lemos <rtl00@yahoo.com>
2018-07-23 18:58:15 -0700

    Docstrings for gammatau

--- R/gammatau.R
+++ R/gammatau.R
@@ -1,4 +1,20 @@
-get_priors = function(dpc_grid, precision_diagonal_value = 1e-6, precision_nearest_neighbor_factor = 0) {
+#' Build basic priors for gamma and tau
+#'
+#' @param dpc_grid Discrete Process Convolution grid
+#' @param precision_diagonal_value Diagonal value for gamma's prior precision matrix
+#' @param precision_nearest_neighbor_factor Off-diagonal factor for gamma's prior precision matrix, in (0,1)
+#'
+#' @return List with prior mean and precision for gamma, and prior shape and rate parameters for tau
+#' @export
+#'
+#' @examples
+#' dpc_grid = get_grid(c(0,1), c(1,2), 1)
+#' priors = get_priors(dpc_grid, 5, 0.4)
+#' priors$gamma$precision
+#' (prior_variance = solve(priors$gamma$precision))
+#' 
+get_priors = function(dpc_grid, precision_diagonal_value = 1e-6, 
+                      precision_nearest_neighbor_factor = 0) {
   ngridpoints = nrow(dpc_grid$coord)
   precision = if (precision_nearest_neighbor_factor == 0)
     Diagonal(ngridpoints, x = precision_diagonal_value)
@@ -7,6 +23,18 @@ get_priors = function(dpc_grid, precision_diagonal_value = 1e-6, precision_neare
   list(gamma = list(mean = rep(0, ngridpoints), precision = precision), tau = list(a = 2, b = 2))
 }
 
+#' Construct gamma's precision matrix for a given DPC grid
+#'
+#' @param dpc_grid Discrete Process Convolution grid
+#' @param diag_value Diagonal value in precision matrix
+#' @param nearest_neighbor_factor Off-diagonal factor for adjacent gridpoints
+#'
+#' @return A sparse precision matrix
+#'
+#' @examples
+#' dpc_grid = get_grid(c(0,1), c(1,2), 1)
+#' get_prior_precision_matrix(dpc_grid, 3, 0.2)
+#' 
 get_prior_precision_matrix = function(dpc_grid, diag_value, nearest_neighbor_factor) {
   nlon = length(dpc_grid$lon)
   nlat = length(dpc_grid$lat)
@@ -62,26 +90,94 @@ get_prior_precision_matrix = function(dpc_grid, diag_value, nearest_neighbor_fac
   }
 }
 
+#' Compute the full conditional (given tau, rho, and data) precision of gamma
+#'
+#' @param prior_precision Prior precision matrix for gamma
+#' @param kernel_matrix Discrete Process Convolution kernel matrix (fixed values of rho)
+#' @param tau Sampled value of tau
+#'
+#' @return Full conditional precision of gamma
+#' @export
+#'
+#' @examples
+#' get_posterior_precision(Matrix::Diagonal(2, 3), list(mat = Matrix::Diagonal(2, 1)), 5)
+#' 
 get_posterior_precision = function(prior_precision, kernel_matrix, tau) {
   prior_precision + tau * Matrix::crossprod(kernel_matrix$mat)
 }
 
+#' Compute the full conditional (given tau, rho, and data) mean of gamma
+#'
+#' @param prior_mean Prior mean of gamma
+#' @param prior_precision Prior precision of gamma
+#' @param posterior_precision Full conditional precision of gamma
+#' @param kernel_matrix DPC kernel matrix
+#' @param tau Sampled value of tau
+#' @param y Data
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' km = list(mat = Matrix::Diagonal(2, 1))
+#' prior_pr = Matrix::Diagonal(2, 0.3)
+#' tau = 5
+#' post_pr = get_posterior_precision(prior_pr, km, tau)
+#' get_posterior_mean(prior_mean = rep(0, 2), prior_precision = prior_pr, 
+#'                    posterior_precision = post_pr, kernel_matrix = km, tau = tau, y = c(0.9, 0.7))
 get_posterior_mean = function(prior_mean, prior_precision, posterior_precision, kernel_matrix, tau, y) {
   d = prior_precision %*% prior_mean + tau * Matrix::crossprod(kernel_matrix$mat, y)
   Matrix::solve(posterior_precision, d)
 }
 
+#' Sample gamma from its full conditional distribution
+#'
+#' @param prior_mean Prior mean of gamma
+#' @param prior_precision Prior precision of gamma
+#' @param kernel_matrix DPC kernel matrix
+#' @param tau Sampled value of tau
+#' @param y Observation value(s)
+#' @param return_mean Provide full conditional mean as pseudo-sample?
+#'
+#' @return Either the full conditional mean of gamma or a sample of it
+#' @export
+#'
+#' @examples
+#' km = list(mat = Matrix::Diagonal(2, 1))
+#' prior_pr = Matrix::Diagonal(2, 0.3)
+#' tau = 5
+#' post_pr = get_posterior_precision(prior_pr, km, tau)
+#' get_gamma_sample(prior_mean = rep(0, 2), prior_precision = prior_pr, 
+#'                  kernel_matrix = km, tau = tau, y = c(0.9, 0.7), return_mean = TRUE)
+#' 
 get_gamma_sample = function(prior_mean, prior_precision, kernel_matrix, tau, y, return_mean = FALSE) {
   posterior_precision = get_posterior_precision(prior_precision, kernel_matrix, tau)
   posterior_mean = get_posterior_mean(prior_mean, prior_precision, posterior_precision, kernel_matrix, tau, y)
   n = nrow(posterior_precision)
   R = Matrix::chol(posterior_precision)
   if (return_mean)
-    posterior_mean
+    as.numeric(posterior_mean)
   else
     as.numeric(solve(R, rnorm(n)) + posterior_mean)
 }
 
+#' Sample tau from its full conditional distribution
+#'
+#' @param kernel_matrix DPC kernel matrix
+#' @param gamma Sample of gamma
+#' @param y Observation values
+#' @param prior_tau_a Prior shape parameter
+#' @param prior_tau_b Prior rate parameter
+#' @param return_mean Provide full conditional mean as pseudo-sample?
+#'
+#' @return Either the full conditional mean of tau or a sample of it
+#' @export
+#'
+#' @examples
+#' km = list(mat = Matrix::Diagonal(2, 1))
+#' get_tau_sample(kernel_matrix = km, gamma = c(0.849, 0.66), y = c(0.9, 0.7), 
+#'                prior_tau_a = 2, prior_tau_b = 2, return_mean = TRUE)
+#'                  
 get_tau_sample = function(kernel_matrix, gamma, y, prior_tau_a, prior_tau_b, return_mean = FALSE) {
   sse = sum((y - kernel_matrix$mat %*% gamma) ** 2)
   posterior_tau_a = prior_tau_a + 0.5 * length(y)
