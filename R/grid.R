967aedb7dd450aeda3728777c864956d0741df9a
Ricardo Silva Tavares de Lemos <rtl@ricardos-mbp.attlocal.net>
2018-07-23 18:18:56 -0700

    Docstring of grid

--- R/grid.R
+++ R/grid.R
@@ -1,3 +1,14 @@
+#' Produce a regular Discrete Process Convolution grid.
+#'
+#' @param longitude_bounds Minimum and maximum longitude
+#' @param latitude_bounds Minimum and maximum latitude
+#' @param spacing Grid resolution, i.e., distance between adjacent gridpoints
+#'
+#' @return List with gridded coodinates, as well as metadata
+#' @export
+#'
+#' @examples
+#' get_grid(c(-1,1), c(2,4), 1)
 get_grid = function(longitude_bounds, latitude_bounds, spacing) {
   lon = seq(longitude_bounds[1], longitude_bounds[2], by = spacing)
   lat = seq(latitude_bounds[1], latitude_bounds[2], by = spacing)
