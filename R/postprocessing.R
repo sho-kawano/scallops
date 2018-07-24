4084a3425c1d9fd555b8240e12d71c415b5168af
Ricardo Silva Tavares de Lemos <rtl00@yahoo.com>
2018-07-23 19:55:43 -0700

    Docstrings for postprocessing functions

--- R/postprocessing.R
+++ R/postprocessing.R
@@ -14,6 +14,16 @@ get_gridpoint_influence = function(dpc_grid, lat, lon, fit = NULL) {
   df
 }
 
+#' Obtain dataframe of kernel ellipses centered at the DPC gridpoints
+#'
+#' @param dpc_grid Discrete Process Convolution grid
+#' @param fit Fitted model
+#' @param eval Size of ellipses: k[s_i, s_j, \phi] = eval
+#'
+#' @return Dataframe with coordinates of points that are points in the ellipses; gp identifies the gridpoint
+#' @export
+#'
+#' @examples
 get_ellipses = function(dpc_grid, fit, eval) {
   angles = seq(0, 2 * pi, length = 100)
   ngridpoints = nrow(dpc_grid$coord)
@@ -34,6 +44,24 @@ get_ellipses = function(dpc_grid, fit, eval) {
   out
 }
 
+#' Get posterior predictive mean estimates at location(s) in the domain
+#'
+#' @param s Locations
+#' @param dpc_grid Discrete Process Convolution grid
+#' @param fit Fitted model
+#'
+#' @return Data frame with coordinates and posterior predictive means
+#' @export
+#'
+#' @examples
+#' s = data.frame(lat = rep(0, 3), lon = c(-1:1))
+#' y = c(1,1,0)
+#' dpc_grid = get_grid(c(-1,1), c(0,0), spacing = 2)
+#' priors = get_priors(dpc_grid)
+#' iso_kernel_matrix = get_kernel_matrix(s, dpc_grid)
+#' fit = get_mcmc(s, dpc_grid, y, 10, 1000, priors, 100, 1)
+#' cbind(get_estimates(s, dpc_grid, fit), obs = y)
+#' 
 get_estimates = function(s, dpc_grid, fit) {
   slist = get_mat2list(s)
   gamma_mat = if (class(fit$gamma) == 'dgeMatrix')
@@ -51,11 +79,52 @@ get_estimates = function(s, dpc_grid, fit) {
   df
 }
 
+#' Get a point estimate for a single location
+#'
+#' @param lon Longitude of point
+#' @param lat Latitude of point
+#' @param dpc_grid Discrete Process Convolution grid
+#' @param fit Fitted model
+#'
+#' @return Posterior predictive mean at given location
+#' @export
+#'
+#' @examples
+#' s = data.frame(lat = rep(0, 3), lon = c(-1:1))
+#' y = c(1,1,0)
+#' dpc_grid = get_grid(c(-1,1), c(0,0), spacing = 2)
+#' priors = get_priors(dpc_grid)
+#' iso_kernel_matrix = get_kernel_matrix(s, dpc_grid)
+#' fit = get_mcmc(s, dpc_grid, y, 10, 1000, priors, 100, 1)
+#' get_point_estimate(lon = s$lon[1], lat = s$lat[1], dpc_grid = dpc_grid, fit = fit)
+#' 
 get_point_estimate = function(lon, lat, dpc_grid, fit) {
   s = data.frame(lon = lon, lat = lat)
   get_estimates(s = s, dpc_grid = dpc_grid, fit = fit)
 }
 
+#' Plot gridded posterior predictive means
+#'
+#' @param obs_coord Coordinates of original observation locations
+#' @param dpc_grid Discrete Process Convolution grid
+#' @param fit Fitted model
+#' @param fineness Resolution of gridded means (1 = coarse, 10 = fine)
+#'
+#' @return Gridded estimates within the domain defined by the observation locations (extapolation is dangerous)
+#' @export
+#'
+#' @examples
+#' set.seed(5)
+#' s = data.frame(lat = runif(3, min = 0, max = 1), lon = runif(3, -1, 1))
+#' y = c(1,1,0)
+#' dpc_grid = get_grid(c(-1,1), c(-1,1), spacing = 2)
+#' priors = get_priors(dpc_grid)
+#' iso_kernel_matrix = get_kernel_matrix(s, dpc_grid)
+#' fit = get_mcmc(s, dpc_grid, y, 10, 1000, priors, 100, 1)
+#' gr = get_gridded_estimates(obs_coord = s, dpc_grid = dpc_grid, fit = fit, fineness = 10)
+#' plot(gr$lon, gr$lat, cex = 0.1 + 4 * gr$z, xlim = c(-1, 1), ylim = c(0, 1))
+#' points(s$lon, s$lat, pch = 19, cex = 0.1 + 4 * y, col = 'red')
+#' 
 get_gridded_estimates = function(obs_coord, dpc_grid, fit, fineness) {
   obs_mat = get_list2mat(obs_coord)
   triangulation = tri.mesh(obs_mat$lon, obs_mat$lat, duplicate="remove")
@@ -66,31 +135,76 @@ get_gridded_estimates = function(obs_coord, dpc_grid, fit, fineness) {
   get_estimates(s = s, dpc_grid = dpc_grid, fit = fit)
 }
 
+
+#' Plot the influence of a gridded Gaussian variable on the interpolation
+#'
+#' @param dpc_grid Discrete Process Convolution grid
+#' @param lat Latitute of desired gridpoint
+#' @param lon Longitude of desired gridpoint
+#' @param fit (Optional) fitted model
+#'
+#' @return A ggplot showing the weight of the selected variable on the fit
+#' @export
+#'
+#' @examples
+#' get_influence_plot(get_grid(c(-1,1), c(-1,1), spacing = 2), -1, -1, fit = NULL)
 get_influence_plot = function(dpc_grid, lat, lon, fit = NULL) {
   df = get_gridpoint_influence(dpc_grid, lat, lon, fit)
   ggplot() + geom_raster(data = df[df$z > 0,], aes(x = lon, y = lat, fill = z))  +
-    geom_point(data = scallop, aes(x = longitude, y = latitude, size = logcatch), shape = 1) +
     geom_point(data = dpc_grid$coord, aes(x = lon, y = lat), shape = 3, color = 'red') +
     coord_fixed(ratio = 1) +
     scale_fill_gradient(low = "light gray", high = "dark blue", space = "Lab",
                         na.value = "grey50", guide = "colourbar", aesthetics = "fill")
 }
 
+#' Plot kernel ellipses centered at the DPC gridpoints
+#'
+#' @param dpc_grid Discrete Process Convolution grid
+#' @param fit Fitted model
+#' @param eval Size of ellipses: k[s_i, s_j, \phi] = eval
+#'
+#' @return Plot of ellipses
+#' @export
+#'
+#' @examples
+#' s = expand.grid(lat = -1:1, lon=-1:1)
+#' y = c(1,0,0,0,1,0,0,0,1)
+#' dpc_grid = get_grid(c(-1,1), c(-1,1), spacing = 2)
+#' priors = get_priors(dpc_grid)
+#' iso_kernel_matrix = get_kernel_matrix(s, dpc_grid)
+#' fit = get_mcmc(s, dpc_grid, y, 10, 1000, priors, 100, 1)
+#' get_interpolation_plot(s, dpc_grid, fit)
+#' get_ellipses_plot(dpc_grid, fit)
 get_ellipses_plot = function(dpc_grid, fit, eval = 0.7) {
   el = get_ellipses(dpc_grid, fit, eval)
   ggplot() + geom_path(data = el, aes(x = lon, y = lat, group = gp), color = 'red') +
-    geom_point(data = scallop, aes(x = longitude, y = latitude, size = logcatch), shape = 1) +
     geom_point(data = dpc_grid$coord, aes(x = lon, y = lat), shape = 3, color = 'red') +
     coord_fixed(ratio = 1)
 }
 
-get_interpolation_plot = function(obs_coord, dpc_grid, fit, fineness = 16, obs_as_labels = FALSE,
+#' Plot the interpolated surface
+#'
+#' @param obs_coord Coordinates of original observations
+#' @param dpc_grid Discrete Process Convolution grid
+#' @param fit Fitted model
+#' @param fineness Resolution of interpolation (1 = coarse, 10 = fine)
+#' @param contour_binwidth Width of bins for contour plot
+#' @param do_grid Plot DPC gridpoints?
+#'
+#' @return Plot of gridded posterior predictive means
+#' @export
+#'
+#' @examples
+#' s = expand.grid(lat = -1:1, lon=-1:1)
+#' y = c(1,0,0,0,1,0,0,0,1)
+#' dpc_grid = get_grid(c(-1,1), c(-1,1), spacing = 2)
+#' priors = get_priors(dpc_grid)
+#' iso_kernel_matrix = get_kernel_matrix(s, dpc_grid)
+#' fit = get_mcmc(s, dpc_grid, y, 10, 1000, priors, 100, 1)
+#' get_interpolation_plot(s, dpc_grid, fit, contour_binwidth = 0.1)
+get_interpolation_plot = function(obs_coord, dpc_grid, fit, fineness = 16,
                                   contour_binwidth = NULL, do_grid = TRUE) {
-  df = get_gridded_estimates(obs_coord = scallop, dpc_grid = dpc_grid, fit = fit, fineness)
-  pts = if (obs_as_labels)
-    geom_text(data = scallop, aes(x = longitude, y = latitude, label = round(logcatch)))
-  else
-    geom_point(data = scallop, aes(x = longitude, y = latitude, size = logcatch), shape = 1)
+  df = get_gridded_estimates(obs_coord = obs_coord, dpc_grid = dpc_grid, fit = fit, fineness)
   ctr = if (is.null(contour_binwidth))
     NULL
   else {
@@ -105,5 +219,5 @@ get_interpolation_plot = function(obs_coord, dpc_grid, fit, fineness = 16, obs_a
     scale_fill_gradient2(low = "black", mid = 'light blue', high = "white", space = "Lab", 
                          midpoint = mean(df$z), na.value = "black", 
                          guide = "colourbar", aesthetics = "fill") +
-    ctr + pts + grd + coord_fixed(ratio = 1)
+    ctr + grd + coord_fixed(ratio = 1)
 }
